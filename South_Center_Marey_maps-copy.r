b345eb5a9a0e1d598e077178eaba96f44701cf6a
Matt Wersebe <matthew.wersebe@ou.edu>
2021-11-22 08:54:46 -0600

    Update South_Center_Marey_maps-copy.r
    edit the marey maps and make new figure one.

--- South_Center_Marey_maps-copy.r
+++ South_Center_Marey_maps-copy.r
@@ -2,10 +2,11 @@
 #Matthew Wersebe 3-14-2021
 #South Center 13B LOD 26 Linkage Map.
 ######################################
+
 library(tidyverse)
 library(ggplot2)
 library(ggpubr)
-setwd("/home/matt/Desktop/Linkage_plots")
+setwd("/home/weider/Genome_assemblies/Marey_maps")
 
 #Chromosome One:
 CHR1 <- read.table("SC_map_Chrom01-marey.txt", header=T)
@@ -15,17 +16,18 @@ CHR1 <- read.table("SC_map_Chrom01-marey.txt", header=T)
 chrom1 <- ggplot(CHR1, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 1 Marey Map")+
+  ggtitle("LG 1")+
   scale_color_identity(guide = "legend", labels = c("Female Map", "Male Map"))+
   xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+
-  theme_light()
+  theme_light() + theme(axis.title.x=element_blank(),
+                        axis.text.x=element_blank(), legend.title = element_blank(), axis.title.y=element_blank())
 chrom1
 #run window recomb rate estimates
 Window_size = 1000000
 Step_size = 10000
 recombination_1 <- Recomb_rate(Data = CHR1, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_1 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_1 <- ggplot(recombination_1, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
   ylab("R (cM/MB)")+
@@ -45,15 +47,16 @@ CHR2 <- read.table("SC_map_Chrom02-marey.txt", header=T)
 chrom2 <- ggplot(CHR2, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 2 Marey Map")+
+  ggtitle("LG 2")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+
-  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+ theme_light()
+  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+ theme_light()+theme(axis.title.x=element_blank(),
+                                                                                     axis.text.x=element_blank(), legend.title = element_blank(), axis.title.y=element_blank())
 chrom2
 #run window recomb rate estimates
 
 recombination_2 <- Recomb_rate(Data = CHR2, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_2 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_2 <- ggplot(recombination_2, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
   ylab("R (cM/MB)")+
@@ -73,18 +76,19 @@ CHR03 <- read.table("SC_map_Chrom03-marey.txt", header=T)
 chrom3 <- ggplot(CHR03, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 3 Marey Map")+
+  ggtitle("LG 3")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+
-  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+theme_light()
+  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+theme_light()+ theme(axis.title.x=element_blank(),
+                                                                                     axis.text.x=element_blank(), legend.title = element_blank(), axis.title.y=element_blank())
 
 #run window recomb rate estimates
 
 recombination_3 <- Recomb_rate(Data = CHR03, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_3 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_3 <- ggplot(recombination_3, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
-  ggtitle("LG 3 Recombination")+
+  ggtitle("LG 3")+
   xlab("Phyiscal Position (MB)")+ ylab("R (cM/MB)")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+theme_light()+ theme(axis.title.x=element_blank(),
                                                                                             axis.text.x=element_blank(),
@@ -99,15 +103,16 @@ CHR04 <- read.table("SC_map_Chrom04-marey.txt", header=T)
 chrom4 <- ggplot(CHR04, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 4 Marey Map")+
+  ggtitle("LG 4")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+
-  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+theme_light()
+  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+theme_light()+theme(axis.title.x=element_blank(),
+                                                                                    axis.text.x=element_blank(), legend.title = element_blank(), axis.title.y=element_blank())
 
 #run window recomb rate estimates
 
 recombination_4 <- Recomb_rate(Data = CHR04, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_4 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_4 <- ggplot(recombination_4, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
   ggtitle("LG 4 Recombination")+
@@ -125,15 +130,16 @@ CHR05 <- read.table("SC_map_Chrom05-marey.txt", header=T)
 chrom5 <- ggplot(CHR05, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 5 Marey Map")+
+  ggtitle("LG 5")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+
-  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+theme_light()
+  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+theme_light()+theme(axis.title.x=element_blank(),
+                                                                                    axis.text.x=element_blank(), legend.title = element_blank(), axis.title.y=element_blank())
 
 #run window recomb rate estimates
 
 recombination_5  <- Recomb_rate(Data = CHR05, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_5 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_5 <- ggplot(recombination_5, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
   ggtitle("LG 5 Recombination")+
@@ -151,15 +157,16 @@ CHR06 <- read.table("SC_map_Chrom06-marey.txt", header=T)
 chrom6 <- ggplot(CHR06, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 6 Marey Map")+
+  ggtitle("LG 6")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+
-  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+ theme_light()
+  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+ theme_light()+theme(axis.title.x=element_blank(),
+                                                                                     axis.text.x=element_blank(), legend.title = element_blank(), axis.title.y=element_blank())
 
 #run window recomb rate estimates
 
 recombination_6 <- Recomb_rate(Data = CHR06, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_6 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_6 <- ggplot(recombination_6, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
   ggtitle("LG 6 Recombination")+
@@ -177,15 +184,16 @@ CHR07 <- read.table("SC_map_Chrom07-marey.txt", header=T)
 chrom7 <- ggplot(CHR07, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 7 Marey Map")+
+  ggtitle("LG 7")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+
-  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+theme_light()
+  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)")+theme_light()+theme(axis.title.x=element_blank(),
+                                                                                    axis.text.x=element_blank(), legend.title = element_blank(), axis.title.y=element_blank())
 
 #run window recomb rate estimates
 
 recombination_7 <- Recomb_rate(Data = CHR07, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_7 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_7 <- ggplot(recombination_7, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
   ggtitle("LG 7 Recombination")+
@@ -203,15 +211,16 @@ CHR08 <- read.table("SC_map_Chrom08-marey.txt", header=T)
 chrom8 <- ggplot(CHR08, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 8 Marey Map")+
+  ggtitle("LG 8")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+
-  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)") + theme_light()
+  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)") + theme_light()+theme(axis.title.x=element_blank(),
+                                                                                      axis.text.x=element_blank(), legend.title = element_blank(), axis.title.y=element_blank())
 
 #run window recomb rate estimates
 
 recombination_8 <- Recomb_rate(Data = CHR08, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_8 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_8 <- ggplot(recombination_8, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
   ggtitle("LG 8 Recombination")+
@@ -229,15 +238,16 @@ CHR09 <- read.table("SC_map_Chrom09-marey.txt", header=T)
 chrom9 <- ggplot(CHR09, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 9 Marey Map")+
+  ggtitle("LG 9")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+
-  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)") +theme_light()
+  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)") +theme_light()+theme(axis.title.x=element_blank(),
+                                                                                     axis.text.x=element_blank(), legend.title = element_blank(), axis.title.y=element_blank())
 
 #run window recomb rate estimates
 
 recombination_9 <- Recomb_rate(Data = CHR09, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_9 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_9 <- ggplot(recombination_9, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
   ggtitle("LG 9 Recombination")+
@@ -255,15 +265,16 @@ CHR10 <- read.table("SC_map_Chrom10-marey.txt", header=T)
 chrom10 <- ggplot(CHR10, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 10 Marey Map")+
+  ggtitle("LG 10")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+
-  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)") +theme_light()
+  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)") +theme_light()+theme(axis.title.x=element_blank(),
+                                                                                    axis.text.x=element_blank(), legend.title = element_blank(), axis.title.y=element_blank())
 
 #run window recomb rate estimates
 
 recombination_10 <- Recomb_rate(Data = CHR10, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_10 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_10 <- ggplot(recombination_10, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
   ggtitle("LG 10 Recombination")+
@@ -281,15 +292,16 @@ CHR11 <- read.table("SC_map_Chrom11-marey.txt", header=T)
 chrom11 <- ggplot(CHR11, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 11 Marey Map")+
+  ggtitle("LG 11")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+
-  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)") + theme_light()
+  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)") + theme_light()+theme(axis.title.x=element_blank(),
+                                                                                      axis.text.x=element_blank(),legend.title = element_blank(), axis.title.y=element_blank())
 
 #run window recomb rate estimates
 
 recombination_11 <- Recomb_rate(Data = CHR11, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_11 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_11 <- ggplot(recombination_11, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
   ggtitle("LG 11 Recombination")+
@@ -307,15 +319,16 @@ CHR12 <- read.table("SC_map_Chrom12-marey.txt", header=T)
 chrom12 <- ggplot(CHR12, aes(x=Physical_position)) + 
   geom_point(aes(y=cM_male, color= "steelblue"))+
   geom_point(aes(y=cM_female, color= "darkred"))+
-  ggtitle("LG 12 Marey Map")+
+  ggtitle("LG 12")+
   scale_color_identity(guide = "legend", labels = c("Female", "Male"))+
-  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)") +theme_light()
+  xlab("Phyiscal Position (MB)")+ ylab("Genetic Position (cM)") +theme_light()+theme(axis.title.x=element_blank(),
+                                                                                     axis.text.x=element_blank(), legend.title = element_blank(), axis.title.y=element_blank())
 
 #run window recomb rate estimates
 
 recombination_12 <- Recomb_rate(Data = CHR12, Window_Size = Window_size, Step_Size = Step_size)
 #Plot
-rec_rate_12 <- ggplot(recombination, aes(x=V3)) + 
+rec_rate_12 <- ggplot(recombination_12, aes(x=V3)) + 
   geom_line(aes(y = V1, color= "steelblue"))+
   geom_line(aes(y = V2, color = "darkred"))+
   ggtitle("LG 12 Recombination")+
@@ -329,43 +342,50 @@ rec_rate_12 <- ggplot(recombination, aes(x=V3)) +
 ##################################################################################################
 # Marey Map Stack
 
-ggarrange(chrom1, chrom2, chrom3, chrom4, chrom5, chrom6, chrom7, chrom8, chrom9, chrom10, chrom11, chrom12, ncol = 3, nrow = 4, legend = "bottom", common.legend = T)
+marey_stack <- ggarrange(chrom1, chrom2, chrom3, chrom4, chrom5, chrom6, chrom7, chrom8, chrom9, chrom10, chrom11, chrom12, ncol = 3, nrow = 4, legend = "top", common.legend = T)
+marey_stack 
 
+annotate_figure(marey_stack, top = text_grob("Marey Maps", color = "black", face = "bold", size = 20), bottom = text_grob("Physical Position (MB)", color = "black", size = 15), 
+                left = text_grob("Genetic Position (cM)", color = "black", size =15, rot = 90))
+               
 
 ############################################################################
 #Linkage Map Plots:
 # 3-17-2021
 #SC 13B LOD26 Linkage Map
-library(LinkageMapView)
-setwd("/home/matt/Desktop/Linkage_plots")
-male_map <- read.table("SC_map_13BLOD26-mapviewermale.txt", header= T, )
-female_map <- read.table("SC_map_13BLOD26-mapviewerfemale.txt", header= T, )
+## Not RUN ##
+
+#library(LinkageMapView)
+#setwd("/home/matt/Desktop/Linkage_plots")
+#male_map <- read.table("SC_map_13BLOD26-mapviewermale.txt", header= T, )
+#female_map <- read.table("SC_map_13BLOD26-mapviewerfemale.txt", header= T, )
 #male map
-outfile = file.path("/home/matt/Desktop/Linkage_plots", "SC_13B_LOD26_male.pdf")
+#outfile = file.path("/home/matt/Desktop/Linkage_plots", "SC_13B_LOD26_male.pdf")
 
-maxpos <- floor(max(male_map$position[male_map$Group == "CHR01"]))
-at.axis <- seq(0, maxpos)
+#maxpos <- floor(max(male_map$position[male_map$Group == "CHR01"]))
+#at.axis <- seq(0, maxpos)
 
 
-axlab <- vector() 
+#axlab <- vector() 
 
-for (lab in 0:maxpos) {if (!lab %% 10){axlab <-c(axlab, lab)} else {axlab <-c(axlab, NA)}}
+#for (lab in 0:maxpos) {if (!lab %% 10){axlab <-c(axlab, lab)} else {axlab <-c(axlab, NA)}}
 
-lmv.linkage.plot(male_map, outfile, mapthese = c("CHR01", "CHR02", "CHR03", "CHR04", "CHR05", "CHR06", "CHR07", "CHR08", "CHR09", "CHR10", "CHR11", "CHR12"), dupnbr=TRUE, cex.axis = 1, at.axis = at.axis, labels.axis = axlab)
+#lmv.linkage.plot(male_map, outfile, mapthese = c("CHR01", "CHR02", "CHR03", "CHR04", "CHR05", "CHR06", "CHR07", "CHR08", "CHR09", "CHR10", "CHR11", "CHR12"), dupnbr=TRUE, cex.axis = 1, at.axis = at.axis, labels.axis = axlab)
 
 #female map
-maxpos <- floor(max(female_map$position[male_map$Group == "CHR01"]))
-at.axis <- seq(0, maxpos)
+#maxpos <- floor(max(female_map$position[male_map$Group == "CHR01"]))
+#at.axis <- seq(0, maxpos)
 
 
-axlab <- vector() 
+#axlab <- vector() 
 
-for (lab in 0:maxpos) {if (!lab %% 10){axlab <-c(axlab, lab)} else {axlab <-c(axlab, NA)}}
+#for (lab in 0:maxpos) {if (!lab %% 10){axlab <-c(axlab, lab)} else {axlab <-c(axlab, NA)}}
 
 
-outfile_f = file.path("/home/matt/Desktop/Linkage_plots", "SC_13B_LOD26_female.pdf")
-lmv.linkage.plot(female_map, outfile_f, mapthese = c("CHR01", "CHR02", "CHR03", "CHR04", "CHR05", "CHR06", "CHR07", "CHR08", "CHR09", "CHR10", "CHR11", "CHR12"), dupnbr =TRUE, cex.axis = 1, at.axis = at.axis, labels.axis = axlab)
-
+#outfile_f = file.path("/home/matt/Desktop/Linkage_plots", "SC_13B_LOD26_female.pdf")
+#lmv.linkage.plot(female_map, outfile_f, mapthese = c("CHR01", "CHR02", "CHR03", "CHR04", "CHR05", "CHR06", "CHR07", "CHR08", "CHR09", "CHR10", "CHR11", "CHR12"), dupnbr =TRUE, cex.axis = 1, at.axis = at.axis, labels.axis = axlab)
+## Not run ##
+###########################################################################################
 ###########################################################################################
 #Relationship of estimated phyiscal and genetic lengths of chromosomes
 
